7ef020b0929b110de8cc933d380ed96ac0081e2e
Jonathon <jon@j3z.org>
2019-02-21 21:33:42 -0800

    tabs/spaces mix = nothx

--- src/GeoXPlanet.py
+++ src/GeoXPlanet.py
@@ -19,29 +19,29 @@ defaultConfig = os.path.join(GXPDIR,"GeoXPlanet.conf")
 configfp = None
 
 try:
-	configfp = open(defaultConfig, 'r')
+    configfp = open(defaultConfig, 'r')
 except IOError:
-	print "I wasn't able to locate your GeoXPlanet config file."
-	print "Would you like for me to create the geoxplanet directory and default files for you now?"
-	print "The geoxplanet directory will be: %s" % GXPDIR
-	print "and all files will be created within the above directory"
-	resp = raw_input("yes or no? ")
-	if resp.lower() == 'yes':
-		print "Creating default geoxplanet directory and files"
-		print "Creating directory: %s" % GXPDIR
-		if not os.path.isdir(CONFDIR):
-			try:
-				os.mkdir(CONFDIR)
-			except OSError, err:
-				print err
-				sys.exit()
-		try:
-			os.mkdir(GXPDIR)
-		except OSError, err:
-			print err
-			sys.exit()
-		configfp = open(defaultConfig, 'w')
-		configfp.write("""
+    print "I wasn't able to locate your GeoXPlanet config file."
+    print "Would you like for me to create the geoxplanet directory and default files for you now?"
+    print "The geoxplanet directory will be: %s" % GXPDIR
+    print "and all files will be created within the above directory"
+    resp = raw_input("yes or no? ")
+    if resp.lower() == 'yes':
+        print "Creating default geoxplanet directory and files"
+        print "Creating directory: %s" % GXPDIR
+        if not os.path.isdir(CONFDIR):
+            try:
+                os.mkdir(CONFDIR)
+            except OSError, err:
+                print err
+                sys.exit()
+        try:
+            os.mkdir(GXPDIR)
+        except OSError, err:
+            print err
+            sys.exit()
+        configfp = open(defaultConfig, 'w')
+        configfp.write("""
 [General]
 ; number of seconds between reading connection info
 DELAY=15
@@ -55,23 +55,23 @@ VERSION=0.99
 PLATFORM=%s
 GXPDIR=%s
 """ % (sys.platform, GXPDIR))
-		configfp.close()
-		configfp = None
-	else:
-		sys.exit()
+        configfp.close()
+        configfp = None
+    else:
+        sys.exit()
 else:
-	print "Using %s" % (defaultConfig)
+    print "Using %s" % (defaultConfig)
 finally:
-	if configfp is None:
-		configfp = open(defaultConfig, 'r')
-	config = ConfigParser.ConfigParser()
-	config.readfp(configfp)
-	if config.get("Static","VERSION") == GXPVERSION:
-		print "Launching GeoXPlanet-%s" % GXPVERSION
-	else:
-		pass  # TODO handle mis-matched config/script versions?
-	configfp.close()
+    if configfp is None:
+        configfp = open(defaultConfig, 'r')
+    config = ConfigParser.ConfigParser()
+    config.readfp(configfp)
+    if config.get("Static","VERSION") == GXPVERSION:
+        print "Launching GeoXPlanet-%s" % GXPVERSION
+    else:
+        pass  # TODO handle mis-matched config/script versions?
+    configfp.close()
 
 if __name__ == '__main__':
-	program = GeoXPlanet(config)
-	program.run()
+    program = GeoXPlanet(config)
+    program.run()
--- src/trace.py
+++ src/trace.py
@@ -2,36 +2,36 @@ from threading import Thread
 import os, re, sys, socket
 
 class trace(Thread):
-	running = False
-	ipRegex = None
-	ipStr = None
-	traceCommand = None
-	results = None
+    running = False
+    ipRegex = None
+    ipStr = None
+    traceCommand = None
+    results = None
 
-	def __init__(self, ipStr):
-		Thread.__init__(self)
-		self.ipRegex = re.compile("([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*)")
-		self.ipStr = ipStr
-		if sys.platform == 'win32':
-			self.traceCommand = 'tracert'
-		else:
-			self.traceCommand = 'traceroute'
+    def __init__(self, ipStr):
+        Thread.__init__(self)
+        self.ipRegex = re.compile("([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*)")
+        self.ipStr = ipStr
+        if sys.platform == 'win32':
+            self.traceCommand = 'tracert'
+        else:
+            self.traceCommand = 'traceroute'
 
-	def run(self):
-		self.running = True
-		traceproc = os.popen("%s %s" % (self.traceCommand, self.ipStr))
-		result = traceproc.readlines()
-		for line in result:
-			#print line
-			ipMatch = self.ipRegex.search(line)
-			if ipMatch is not None:
-				addr = ipMatch.group(1)
-			self.results = "%s %s" % (self.results, self.ipStr)
-		self.results = "%s %s" % (self.results, self.ipStr)
-		self.running = False
-	
-	def getList(self):
-		return self.results.split(' ')
+    def run(self):
+        self.running = True
+        traceproc = os.popen("%s %s" % (self.traceCommand, self.ipStr))
+        result = traceproc.readlines()
+        for line in result:
+            #print line
+            ipMatch = self.ipRegex.search(line)
+            if ipMatch is not None:
+                addr = ipMatch.group(1)
+            self.results = "%s %s" % (self.results, self.ipStr)
+        self.results = "%s %s" % (self.results, self.ipStr)
+        self.running = False
+    
+    def getList(self):
+        return self.results.split(' ')
 
-	def isRunning(self):
-		return self.running
+    def isRunning(self):
+        return self.running
